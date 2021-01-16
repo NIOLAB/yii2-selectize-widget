c37b2cc8cf90fb05f2d4c75b9c51c794db67158b
lhfi <info@lymac.co.nz>
2021-01-16 18:03:09 +1300

    Support Bootstrap 4
    Use Selectize js from CDN

--- src/SelectizeAsset.php
+++ src/SelectizeAsset.php
@@ -1,31 +1,32 @@
 <?php
 /**
- * @link https://github.com/2amigos/yii2-selectize-widget
- * @copyright Copyright (c) 2013-2017 2amigOS! Consulting Group LLC
- * @license http://opensource.org/licenses/BSD-3-Clause
- */
+* @link https://github.com/2amigos/yii2-selectize-widget
+* @copyright Copyright (c) 2013-2017 2amigOS! Consulting Group LLC
+* @license http://opensource.org/licenses/BSD-3-Clause
+*/
 
 namespace dosamigos\selectize;
 
 use yii\web\AssetBundle;
 
 /**
- * SelectizeAsset
- *
- * @author 2amigos.us <hola@2amigos.us>
- */
+* SelectizeAsset
+*
+* @author 2amigos.us <hola@2amigos.us>
+*/
 class SelectizeAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/selectize/dist';
+	public $basePath = '@webroot';
+	public $baseUrl = '@web';
 
-    public $css = [
-        'css/selectize.bootstrap3.css',
-    ];
-    public $js = [
-        'js/standalone/selectize.js',
-    ];
-    public $depends = [
-        'yii\bootstrap\BootstrapAsset',
-        'yii\web\JqueryAsset',
-    ];
+	public $css = [
+		'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.13.0/css/selectize.bootstrap4.min.css'
+	];
+	public $js = [
+		'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.13.0/js/standalone/selectize.min.js ',
+	];
+	public $depends = [
+		'yii\bootstrap4\BootstrapAsset',
+		'yii\web\JqueryAsset',
+	];
 }
